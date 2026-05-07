345db673aa37418024fc65f07c1d1544a6ad1322
DaShork <nhatminhvo2311@gmail.com>
2026-05-08 00:38:26 +0700

    feat(comment):add comment explaining function and notice

--- Week1/fetchjson/src/index.ts
+++ Week1/fetchjson/src/index.ts
@@ -2,18 +2,11 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+//Định nghĩa biến và loại biến sẽ giúp typescript có thể kiểm tra và thống báo cho người dùng trong trường hợp data trả về từ api sai
 interface Todo {
     id: number;
     title:string;
     completed: boolean;
-}
-
-const logTodo = (id:number, title:string, completed:boolean) => {
-    console.log(`
-        The todo with id : ${id};
-        Has a title of : ${title};
-        Is it completed : ${completed};
-    `)
 }
 
 axios.get(url).then(response => {
@@ -23,6 +16,15 @@ axios.get(url).then(response => {
     const title = todo.title;
     const completed = todo.completed;
 
-    logTodo(id, title, completed)
+    logTodo(id, title, completed); //VD1: nếu chèn completed lên trước title mà không sử dụng anotation thì sẽ không báo lỗi dẫn đến in ra kq lỗi
 
 })
+// Thêm notation khi khai báo các biến nhận vào sẽ giúp cho typescript thông báo khi xảy ra các sự cố liên quan
+// Ví dụ như đặt lộn thứ tự các biến nhận vào (VD1)
+const logTodo = (id:number, title:string, completed:boolean) => {
+    console.log(`
+        The todo with id : ${id};
+        Has a title of : ${title};
+        Is it completed : ${completed};
+    `)
+}
